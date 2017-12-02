34d5bca040513d16d9cb81f89435b461dfb1a2ba
Glauber Silva <yuukiosan@gmail.com>
2017-12-02 11:55:21 -0300

    Atualização script.js

--- glauber/scripjquery.js
+++ glauber/scripjquery.js
@@ -1,1 +1,32 @@
+var valor = 0;
+var verificaLamp1 = 0;
+var verificaLamp2 = 0;
 
+function lampAcesa(event){
+	
+	if (event.target.id == "lamp1" && verificaLamp1 == 0 || event.target.id == "lamp2" && verificaLamp2 == 0){
+		document.getElementById(event.target.id).src = "https://i.stack.imgur.com/ybxlO.jpg";
+	}
+
+}
+
+function lampApaga(event){
+	
+	if (event.target.id == "lamp1" && verificaLamp1 == 0 || event.target.id == "lamp2" && verificaLamp2 == 0){
+		document.getElementById(event.target.id).src = "https://i.stack.imgur.com/b983w.jpg";
+	}
+
+}
+
+function lampQuebra(event){
+
+	if (event.target.id == "lamp1" && verificaLamp1 == 0 || event.target.id == "lamp2" && verificaLamp2 == 0){
+		document.getElementById(event.target.id).src = "https://i.stack.imgur.com/MRjsF.jpg";
+		if(event.target.id == "lamp1"){
+			verificaLamp1 = 1;
+		}else{
+			verificaLamp2 = 1;
+		}
+	}
+	
+}
