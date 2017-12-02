c7898f6e8dd32374a132b0e60404532064f6e5ef
Aluno <ramondecerqueirasilva@gmail.com>
2017-12-02 11:26:07 -0300

    Update archives

--- 02-11/assets/js/index.js
+++ 02-11/assets/js/index.js
@@ -1,6 +1,34 @@
-function button(){
+
+var q = false;
+
+/*function button(){
 	document.getElementById("p3").innerHTML = "Its JS";
 }
 function text(){
 	alert("TROUXA");
+}*/
+
+function quebra(){
+	document.getElementById("q").src = "quebrada.jpg"
+	q = true;
+}
+
+function acende(){
+	if(q == false){
+		document.getElementById("q").src = "acende.jpg"
+	}else{
+		return false;
+	}
+}
+
+function normal(){
+	if(q != true){
+			document.getElementById("q").src = "normal.jpg"
+	}
+}
+
+function fix(){
+	if(q == "quebrada.jpg"){
+		return false;
+	}
 }
