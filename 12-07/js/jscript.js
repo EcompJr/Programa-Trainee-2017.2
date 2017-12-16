1b81d53146a8570eff63718dd13b5707a601e838
ramondcsilva <ramondecerqueirasilva@github.com>
2017-12-16 20:06:29 -0300

    Update
    Update archives.

--- 12-07/js/jscript.js
+++ 12-07/js/jscript.js
@@ -4,12 +4,35 @@ $(document).ready(function(){
 		$("#texto1").show();
 	});
 	$("#botao1").click(function(){
-		$("#botao0").hide();
-		$("#botao1").hide();
-		$("#texto0").hide();
-		$("#texto1").hide();
-		$("#titulo").hide();
-		alert("Dados Enviados!");
+
+		var texto0 = document.getElementById('texto0');
+		var texto1 = document.getElementById('texto1');
+		var string0 = document.getElementById('texto0').value;
+		var string1 = document.getElementById('texto1').value;
+		var soma = string0.length + string1.length;
+		
+		if(soma > 10){
+			$("#botao0").hide();
+			$("#botao1").hide();
+			$("#texto0").hide();
+			$("#texto1").hide();
+			$("#titulo").hide();
+			alert("Valido!");
+		}else{
+			alert("Invalido!");
+		}
 	});
 });
+
+function validaCampo(){
+	var campo = document.getElementById('campo');
+	var txt   = document.getElementById('campo').value;
+	var n     = txt.length;
+	if(n < 6){
+	  alert("O Texto digitado tem " + n + " caracteres");
+	  return false;
+	  campo.focus();
+	}
+  	return true;
+  	}
 
